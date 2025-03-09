a4aa961373843edcac4988cb46a28a808bbd1735
Erick Schiavo <erick.schiavo@hotmail.com>
2025-03-09 10:34:48 -0300

    Fixing Menu and level

--- code/Level.py
+++ code/Level.py
@@ -43,7 +43,7 @@ class Level:
         pass
 
     def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
-        text_font: Font = pygame.font.Sysfont(name="Lucida Sans Typewriter", size= text_size)
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size= text_size)
         text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
         text_rect: Rect = text_surf.get_rect(left=text_pos[0], top=text_pos[1])
         self.window.blit(source=text_surf, dest=text_rect)
